8531e65d08969960b0998e0a443758d5904d4a24
robkadj <robert.kadjinskii@gmail.com>
2023-03-30 18:01:36 +0300

    Added example module's config into main "nginx.conf"

--- configure_default.sh
+++ configure_default.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 ./configure --prefix=/space/projects/nginx-exp/build/
+
+# ./configure --prefix=/space/projects/nginx-exp/build/ --add-dynamic-module=/space/projects/nginx-exp/nginx-hello-world-module
